3c82126ee8f23ec228915df2c00619b162dbc386
milena <milena.wolczynska@onet.eu>
2021-03-23 16:00:20 +0100

    my first simple programs

---
+++ 01_JAVA/src/Casting.java
@@ -0,0 +1,10 @@
+public class Casting {
+    public static void main(String[] args) {
+        //casting/rzutowanie
+        double nn = 99.99989669;
+        int m = (int) nn;
+        int mm = (int) Math.round(nn);
+        System.out.println(m);
+        System.out.println(mm);
+    }
+}
