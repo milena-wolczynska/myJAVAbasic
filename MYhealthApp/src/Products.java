815c59c52cdcfeff1ef015428b9544293adb56cb
milena <milena.wolczynska@onet.eu>
2021-03-28 20:18:32 +0200

    MYhealthApp starting + singleton example

---
+++ MYhealthApp/src/Products.java
@@ -0,0 +1,14 @@
+public class Products {
+    String products1 = "Vegetables";
+    String products2 = "Fruits";
+    String products3 = "Diary products";
+    String products4 = "Bread";
+    String products5 = "Meat";
+    String products6 = "Fish";
+    String products7 = "Cereals";
+    String products8 = "Fats";
+    String products9 = "Legumes";
+    String products10 = "Drinks";
+
+
+}
